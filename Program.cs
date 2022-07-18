1f29e51612a26b717a8b163f760dd73754be94ee
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-07-18 18:56:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookApi.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<BookApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookApiContext") ?? throw new InvalidOperationException("Connection string 'BookApiContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
