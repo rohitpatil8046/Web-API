7f231f10631869fe22c32da6205aebfeffa51d3a
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-07-18 18:57:13 +0530

    Add files via upload

---
+++ BookApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Model;
+
+namespace BookApi.Data
+{
+    public class BookApiContext : DbContext
+    {
+        public BookApiContext (DbContextOptions<BookApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookApi.Model.Crypto> Crypto { get; set; } = default!;
+    }
+}
